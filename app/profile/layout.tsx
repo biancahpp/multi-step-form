4f6567e209185de1cebb8c7a4bf95b9085fed052
biancahpp <biancahprocopio@gmail.com>
2023-05-14 17:29:53 -0600

    add data file plus routes examples

---
+++ app/profile/layout.tsx
@@ -0,0 +1,12 @@
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+        <h2>Hello from profile/layout</h2>
+        {children}
+    </div>
+  )
+}
---
+++ app/profile/student/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <h3>Student route!</h3>;
+  }
