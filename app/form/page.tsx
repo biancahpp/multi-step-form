acec4debb1aae284124747940807bffaa9809703
biancahpp <biancahprocopio@gmail.com>
2023-06-10 15:57:21 -0600

    adjust background image up until tablet breakpoint

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -3,6 +3,9 @@ import styles from "./page.module.scss";
 export default function Page() {
     return (
       <main className={styles.main}>
+        <div className={styles.header}>
+          <h1>Ola</h1>
+        </div>
         Hello world
       </main>
     )
